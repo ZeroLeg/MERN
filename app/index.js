6126ac836a53ebbdbb61c0400406b48225b03682
adrian <karatsumo@gmail.com>
2021-07-18 19:23:23 +0200

    add comments

--- app/index.js
+++ app/index.js
@@ -11,9 +11,11 @@ app.use(morgan('dev'));
 app.use(express.json());
 
 // Routes
+// Aquí tenemos las rutas de la api de express
 app.use('/api/tasks', require('./routes/task.routes'));
 
 // Static files
+// Donde montamos nuestra app de react, en el index.html de la carpeta public
 app.use(express.static(path.join(__dirname, 'public')));
 
 // Starting server
