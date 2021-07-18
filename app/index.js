ca9db95249582a5848e111fb0f94d5be6307e4de
adrian <karatsumo@gmail.com>
2021-07-18 19:22:20 +0200

    preparacion base

--- app/index.js
+++ app/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const morgan = require('morgan');
+const path = require('path');
 const app = express();
 
 // Settings
@@ -8,10 +9,12 @@ app.set('port', process.env.PORT || 3000)
 // Middlewares
 app.use(morgan('dev'));
 app.use(express.json());
+
 // Routes
+app.use('/api/tasks', require('./routes/task.routes'));
 
 // Static files
-
+app.use(express.static(path.join(__dirname, 'public')));
 
 // Starting server
 app.listen( app.get('port'), ()=>{
